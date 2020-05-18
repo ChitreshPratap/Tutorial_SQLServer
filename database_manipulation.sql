e7920b0b7895369c4f108a916b15c77ff0e81fb5
ChitreshPratap <57294516+ChitreshPratap@users.noreply.github.com>
2020-05-18 10:18:45 +0530

    Update database_manipulation.sql
    2 renaming db queries added

--- database_manipulation.sql
+++ database_manipulation.sql
@@ -4,3 +4,13 @@ create database Noble_Public_School
 
 --drop database
 drop database Noble_Public_School
+
+
+--renaming database
+--way1 
+alter database Noble_Public_School
+modify name=NPS
+
+--way2
+execute sp_renameDB 'NPS','NoblePublicSchool'
+sp_renameDB 'NPS','NoblePublicSchool'
