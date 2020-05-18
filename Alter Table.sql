18367350298716d5b486f40f4e3da768b94368ed
ChitreshPratap <57294516+ChitreshPratap@users.noreply.github.com>
2020-05-18 14:49:15 +0530

    Alter Table
    add/remove column
    add/remove constraint
    change data type of column and nullability of column
    rename column

---
+++ Alter Table.sql
@@ -0,0 +1,31 @@
+
+--Adding a new column to existing table
+alter table Course
+add MinimumCourseCredit integer not null constraint df_Course_MinimumCourseCredit default 4,
+	MaximumCourseCredit integer not null constraint df_Course_MaximumCourseCredit default 8
+
+
+--Deleting existing columns from table
+alter table Course
+drop column MinimumCourseCredit,
+			MaximumCourseCredit
+
+--rename column of table
+sp_rename 'Course.MinCourseCredit','MinimumCourseCredit','COLUMN'
+
+
+
+
+--droping constraints from table
+alter table Course
+drop constraint df_Course_MinimumCourseCredit,df_Course_MaximumCourseCredit
+
+--adding new constriant to column
+alter table Course	
+add constraint uq_Course_CourseName unique(CourseName),
+	constraint df_Course_CourseName default 'not defined' for CourseName,
+	constraint ck_Course_CourseName check(len(CourseName)>=3)
+
+--change Datatype and nullability of column
+alter table Course
+alter column Credit nvarchar(50) not null
