6bfaa68c947889d7ec14ba68533af5991703168a
ChitreshPratap <57294516+ChitreshPratap@users.noreply.github.com>
2020-05-21 10:43:32 +0530

    Versus

--- Versus.sql
+++ Versus.sql
@@ -1,8 +1,27 @@
+/*
+
+1.	Stored Procedure VS Functions
+2.	nvarchar VS char VS varchar
+3.	Except VS Not In
+4.	Inline-table values function VS Scalar functions
+5.	Permanent table vs temporary table
+6.	local temporary table vs global temporary table
+
+7.	Derived Table Vs Sub Query
+		Derived Tables
+			must be enclosed within parenthesis and name is provided.
+			can have one/multiple columns
+			derived tables are used only in From clause of outer table.
+
+		Sub-query 
+			must be enclosed within parenthesis.
+			sub-query can have only 1 colun.
+			Sub-Query is used within where clause of outer table mainly to filter out certain rows returned in result set of outer query.
+
+		
+8.	CTE and Derived Tables
+9.	Table Variable Vs Temporary tables
 
 
---1.	Stored Procedure VS Functions
---2.	nvarchar VS char VS varchar
---3.	Except VS Not In
---4.	Inline-table values function VS Scalar functions
-
+		
 
