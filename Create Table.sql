c14b245af87470f3f41c705534eb5c1a69fd426a
abhilash <57294516+ChitreshPratap@users.noreply.github.com>
2021-09-29 17:43:29 +0530

    Documents updated.
    Documents updated.

--- Create Table.sql
+++ Create Table.sql
@@ -1,3 +1,11 @@
+--Creating Tables with constraints :- 
+--	Nullability Constraint
+--	Primary Key
+--	Unique Key
+--	Default Key 
+--	Check Key
+--	Foreign Key
+
 
 Create table Bank
 (
@@ -12,14 +20,25 @@ create table Employee
 --if column name contains space then enclosed that column name inside [ ]	
 	[Employee Id] int not null,
 	[Employee Name] nvarchar(100) not null,
-	[Father Name] nvarchar(100) not null,
+	[Father Name] nvarchar(100) not null,	
 
 --if column name does not contain space then not neccessary to enclosed column name inside [ ]
+--If nullability constraint is not defined then it is null by default.
 	Aadhar nvarchar(20),
-	[BankAccount] nvarchar(20),
-
+	[BankAccount] nvarchar(20) null,
+	DOB date not null,
+	Age int not null 
+		constraint df_employee_age Default(18),
+	
 	constraint pk_employee_id Primary Key ([Employee Id]),
+
 	constraint uq_aadhar Unique (Aadhar),	
+
 	constraint fk_bankAccount_Bank_AccountNumber foreign key ([BankAccount])
-		references Bank([Account Number])
+		references Bank([Account Number]),
+
+	constraint ck_age_gt18 check (Age>18 And Age <=60),
+
+	--default constraint is not applied in this way in create table statement, we should implement default constraint inline
+	--constraint df_employee_age default (18) for [Age]
 )
